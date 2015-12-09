93d57ce1cb2f8b36c67ac082c474fc1ddd014be9
Myles Braithwaite <me@mylesbraithwaite.com>
2015-12-09 11:54:59 -0500

    Adding some testing stuff.

---
+++ tests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import unittest
+
+from timeofday import app
+
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home_page_works(self):
+        rv = self.app.get('/')
+        self.assertTrue(rv.data)
+        self.assertEqual(rv.status_code, 200)
+
+    def test_api_v1_works(self):
+        rv = self.app.get('/api/v1')
+        self.assertTrue(rv.data)
+        self.assertEqual(rv.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
